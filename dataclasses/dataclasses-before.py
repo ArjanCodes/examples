308427d60c26a6bbc5da54ebe4fdfbe1e901f948
Arjan Egges <arjan@quarterfall.com>
2021-03-23 14:49:33 +0100

    Added dataclasses example

---
+++ dataclasses/dataclasses-before.py
@@ -0,0 +1,19 @@
+class Person:
+    name: str
+    job: str
+    age: int
+
+    def __init__(self, name, job, age):
+        self.name = name
+        self.job = job
+        self.age = age
+
+person1 = Person("Geralt", "Witcher", 30)
+person2 = Person("Yennefer", "Sorceress", 25)
+person3 = Person("Yennefer", "Sorceress", 25)
+
+print(id(person2))
+print(id(person3))
+print(person1)
+
+print(person3 == person2)
