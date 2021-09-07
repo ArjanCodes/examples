3d1024f3f451e92cbc95f017edcd5d1c8efc5461
Mark Todisco <marktodisco@gmail.com>
2021-09-07 12:27:50 -0400

    Removed bug from before version of dataset.py

--- datascience_tips/before/src/dataset.py
+++ datascience_tips/before/src/dataset.py
@@ -39,8 +39,8 @@ class MNIST(Dataset):
         self.x = self.data[self.idx].copy().astype(np.float64)
         self.x /= self.TRAIN_MAX
         self.x -= self.TRAIN_NORMALIZED_MEAN
-        self.x /= self.TRAIN_NORMALIZED_MEAN
-        self.x = self.data[self.idx].astype(np.float32)
+        self.x /= self.TRAIN_NORMALIZED_STDEV
+        self.x = self.x.astype(np.float32)
         self.x = torch.from_numpy(self.x)
         self.x = self.x.unsqueeze(0)
 
