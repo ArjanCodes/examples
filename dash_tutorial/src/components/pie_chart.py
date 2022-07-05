702f161fc298fda0ed88b8699edec1cd7fed566a
Mark Todisco <marktodisco@gmail.com>
2022-07-05 11:49:20 -0400

    handle app crash when dropdown selections return no data

--- dash_tutorial/src/components/pie_chart.py
+++ dash_tutorial/src/components/pie_chart.py
@@ -1,3 +1,4 @@
+import i18n
 import pandas as pd
 import plotly.graph_objects as go
 from dash import Dash, dcc, html
@@ -12,7 +13,9 @@ def render(app: Dash) -> html.Div:
         Output(ids.PIE_CHART, "children"),
         Input(ids.RECORDS, "data"),
     )
-    def update_pie_chart(pivot_table_records: list[dict[str, float]]) -> dcc.Graph:
+    def update_pie_chart(pivot_table_records: list[dict[str, float]]) -> html.Div:
+        if len(pivot_table_records) == 0:
+            return html.Div(i18n.t("general.no_data"))
         pivot_table = pd.DataFrame(pivot_table_records)
         pie = go.Pie(
             labels=pivot_table.loc[:, DataSchema.CATEGORY.value],
@@ -22,6 +25,6 @@ def render(app: Dash) -> html.Div:
         fig = go.Figure(data=[pie])
         fig.update_layout(margin={"t": 40, "b": 0, "l": 0, "r": 0})
         fig.update_traces(hovertemplate="%{label}<br>$%{value:.2f}<extra></extra>")
-        return dcc.Graph(figure=fig)
+        return html.Div(dcc.Graph(figure=fig))
 
     return html.Div(id=ids.PIE_CHART)
