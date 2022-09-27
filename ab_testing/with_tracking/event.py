2f360da7b727a83e45cad366d8bf3dd9760b7499
Arjan Egges <arjan@quarterfall.com>
2022-09-27 15:41:25 +0200

    Added event printing.

--- ab_testing/with_tracking/event.py
+++ ab_testing/with_tracking/event.py
@@ -5,6 +5,6 @@ from mixpanel import Mixpanel
 
 def post_event(event_type: str) -> None:
     token = os.getenv("MIXPANEL_TOKEN") or ""
-    print(token)
+    print(f"Sending event {event_type} to Mixpanel.")
     m_panel = Mixpanel(token)
     m_panel.track("ID", event_type)
