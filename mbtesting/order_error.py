0172924e1a54f513f62c5e99f49405c1b11aae4c
Arjan Egges <arjan@quarterfall.com>
2022-11-25 12:45:53 +0100

    Updated stateful testing example.

--- mbtesting/order_error.py
+++ mbtesting/order_error.py
@@ -19,26 +19,22 @@ class Order:
     _total_cache: int = 0
 
     def __post_init__(self) -> None:
-        self._update_total_cache()
+        self._total_cache = sum(li.total for li in self.line_items)
 
     def add_line_item(self, line_item: LineItem) -> None:
         self.line_items.append(line_item)
-        self._update_total_cache()
+        self._total_cache += line_item.total
 
     def remove_line_item(self, line_item: LineItem) -> None:
-        if not line_item in self.line_items:
-            return
+        self._total_cache -= line_item.total
         self.line_items.remove(line_item)
-        self._update_total_cache()
 
     def update_li_quantity(self, line_item: LineItem, quantity: int) -> None:
         if not line_item in self.line_items:
             return
+        self._total_cache -= line_item.total
         line_item.quantity = quantity
-        self._update_total_cache()
-
-    def _update_total_cache(self) -> None:
-        self._total_cache = sum(li.total for li in self.line_items)
+        self._total_cache += line_item.total
 
     @property
     def total(self) -> int:
--- mbtesting/test_order_stateful.py
+++ mbtesting/test_order_stateful.py
@@ -1,19 +1,19 @@
 import unittest
 
 import hypothesis.strategies as st
-from hypothesis.stateful import Bundle, RuleBasedStateMachine, rule
+from hypothesis.stateful import Bundle, RuleBasedStateMachine, precondition, rule
 from order import LineItem, Order
-
-LINE_ITEMS: Bundle[LineItem] = Bundle("line_items")
 
 
 class OrderTest(RuleBasedStateMachine):
-    def __init__(self):
+    line_items: Bundle[LineItem] = Bundle("line_items")
+
+    def __init__(self) -> None:
         super().__init__()
         self.order = Order("John Doe", [])
 
     @rule(
-        target=LINE_ITEMS,
+        target=line_items,
         description=st.text(),
         price=st.integers(),
         quantity=st.integers(),
@@ -21,21 +21,26 @@ class OrderTest(RuleBasedStateMachine):
     def create_line_item(self, description: str, price: int, quantity: int) -> LineItem:
         return LineItem(description, price, quantity)
 
-    @rule(line_item=LINE_ITEMS)
+    @rule(line_item=line_items)
     def add_line_item_to_order(self, line_item: LineItem) -> None:
         self.order.add_line_item(line_item)
 
-    @rule(line_item=LINE_ITEMS)
+    @rule(line_item=line_items)
     def remove_line_item_from_order(self, line_item: LineItem) -> None:
         self.order.remove_line_item(line_item)
 
-    @rule(line_item=LINE_ITEMS, quantity=st.integers())
+    @rule(line_item=line_items, quantity=st.integers())
     def update_line_item_quantity(self, line_item: LineItem, quantity: int) -> None:
         self.order.update_li_quantity(line_item, quantity)
 
     @rule()
     def total_agrees(self) -> None:
         assert sum(li.total for li in self.order.line_items) == self.order.total
+
+    @precondition(lambda self: len(self.order.line_items) == 0)
+    @rule()
+    def total_agrees_zero(self) -> None:
+        assert self.order.total == 0
 
 
 OrderTestCase: unittest.TestCase = OrderTest.TestCase
