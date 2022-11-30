c9781190bf7acf9b9019b15f03fb3db2e3f0f465
Arjan Egges <arjan@quarterfall.com>
2022-11-30 15:06:57 +0100

    Updated Pulumi example.

--- cicd/__main__.py
+++ cicd/__main__.py
@@ -4,32 +4,18 @@ import time
 import pulumi
 from pulumi_gcp import cloudfunctions, storage
 
-# Disable rule for that module-level exports be ALL_CAPS, for legibility.
-# pylint: disable=C0103
-
 # File path to where the Cloud Function's source code is located.
 PATH_TO_SOURCE_CODE = "./functions"
 
 # We will store the source code to the Cloud Function in a Google Cloud Storage bucket.
 bucket = storage.Bucket("channel_api_bucket", location="US", force_destroy=True)
 
-# The Cloud Function source code itself needs to be zipped up into an
-# archive, which we create using the pulumi.AssetArchive primitive.
-assets = {}
-for file in os.listdir(PATH_TO_SOURCE_CODE):
-    location = os.path.join(PATH_TO_SOURCE_CODE, file)
-    asset = pulumi.FileAsset(path=location)
-    assets[file] = asset
-
-archive = pulumi.AssetArchive(assets=assets)
-
 # Create the single Cloud Storage object, which contains all of the function's
 # source code. ("main.py" and "requirements.txt".)
 source_archive_object = storage.BucketObject(
     "channel_api",
-    name=f"main.py-{time.time()}",
     bucket=bucket.name,
-    source=archive,
+    source=pulumi.asset.FileArchive(PATH_TO_SOURCE_CODE),
 )
 
 # Create the Cloud Function, deploying the source we just uploaded to Google
@@ -37,7 +23,6 @@ source_archive_object = storage.BucketObject(
 fxn = cloudfunctions.Function(
     "channel_api",
     entry_point="channel_api",
-    region="us-central1",
     runtime="python310",
     source_archive_bucket=bucket.name,
     source_archive_object=source_archive_object.name,
