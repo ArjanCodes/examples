2085f5e918a0b2fd4f97bd3c21ee0a3dd5ea77a5
Henrique Branco <henrique.ajnbranco@gmail.com>
2023-01-14 16:29:29 -0300

    feat: added pure_fabrication example

---
+++ grasp/09-pure_fabrication_after.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from time import sleep
+
+
+@dataclass
+class PersistentStorage:
+    """Storage records in dictionary."""
+
+    storage: dict[str, int] = field(default_factory=dict)
+
+    def save(self, key, data) -> None:
+        """Add a record to the storage."""
+        self.storage[key] = data
+
+    def load(self, key) -> int:
+        """Get and amout from the storage."""
+        return self.storage[key]
+
+    def show(self) -> None:
+        """Shows all stored records."""
+        print(self.storage)
+
+
+@dataclass
+class Payment:
+    """Payment in an store."""
+
+    amount: int
+    persistent_storage: PersistentStorage
+
+    def pay(self) -> None:
+        """Process payment."""
+        # code to process payment
+        print("Payment processed.")
+
+
+@dataclass
+class PaymentHandler:
+    """Handle the entire payment process."""
+
+    persistent_storage: PersistentStorage
+
+    def handle_payment(self, payment):
+        """Apply payment and store it at the end."""
+        payment.pay()
+        self.persistent_storage.save(datetime.now().isoformat(), payment.amount)
+
+
+def main() -> None:
+
+    persistent_storage = PersistentStorage()
+    payment_handler = PaymentHandler(persistent_storage)
+
+    payment1 = Payment(100, persistent_storage)
+    payment2 = Payment(200, persistent_storage)
+
+    payment_handler.handle_payment(payment1)
+    sleep(1)  # just to get a different timestamp
+    payment_handler.handle_payment(payment2)
+
+    persistent_storage.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ grasp/09-pure_fabrication_before.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from time import sleep
+
+
+@dataclass
+class Payment:
+    """Payment in an store."""
+
+    storage: dict[str, int] = field(default_factory=dict)
+
+    def pay(self, amount) -> None:
+        """Process payment within an value."""
+        # code to process payment
+        print("Payment processed.")
+        self.storage[datetime.now().isoformat()] = amount
+
+    def show_storage_records(self) -> None:
+        """Show all stored payment records."""
+        print(self.storage)
+
+
+def main() -> None:
+
+    payment = Payment()
+
+    payment.pay(100)
+    sleep(1)  # just to get a different timestamp
+    payment.pay(200)
+
+    payment.show_storage_records()
+
+
+if __name__ == "__main__":
+    main()
