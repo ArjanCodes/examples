57f4d92a92c69ad9c33b0e0961772bb2ab728d0e
Arjan Egges <arjan@quarterfall.com>
2023-06-30 16:16:58 +0200

    Updated langchain example to use Pydantic.

--- 2023/langchain/main.py
+++ 2023/langchain/main.py
@@ -1,59 +1,57 @@
 import os
 
-import json
-from typing import Any
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
-    SystemMessagePromptTemplate,
 )
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+
+
+# Define your desired data structure.
+class Country(BaseModel):
+    capital: str = Field(description="capital of the country")
+    name: str = Field(description="name of the country")
+
 
 load_dotenv()
 
 OPENAI_MODEL = "gpt-3.5-turbo"
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-PROMPT_SYSTEM = """
-    You are a RESTful API that only responds in JSON.
-    """
+
 PROMPT_COUNTRY_INFO = """
-    Tell me what the capital is of {country}. If you don't know the country, make up a capital name.
-    The JSON response should an object with a field called "capital" containing the capital of the country.
+    Provide information about {country}.
+    {format_instructions}
     """
-
-
-def to_json(json_str: str) -> object:
-    cleaned_json = json_str.strip().replace('\\"', "'").replace("\\", "\\\\")
-    try:
-        return json.loads(cleaned_json)
-    except json.decoder.JSONDecodeError as err:
-        print("JSONDecodeError: ", err)
-        print("+++++++++++++++")
-        print(cleaned_json)
-        print("+++++++++++++++")
-        return {}
 
 
 def main():
+    # Set up a parser + inject instructions into the prompt template.
+    parser = PydanticOutputParser(pydantic_object=Country)
+
     # setup the chat model
     llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL)
-    system_message = SystemMessagePromptTemplate.from_template(template=PROMPT_SYSTEM)
-    message = HumanMessagePromptTemplate.from_template(template=PROMPT_COUNTRY_INFO)
-    chat_prompt = ChatPromptTemplate.from_messages([system_message, message])
+    message = HumanMessagePromptTemplate.from_template(
+        template=PROMPT_COUNTRY_INFO,
+    )
+    chat_prompt = ChatPromptTemplate.from_messages([message])
 
     # get user input
-    country = input("Enter the name of a country: ")
+    country_name = input("Enter the name of a country: ")
 
     # generate the response
     print("Generating response...")
-    chat_prompt_with_values = chat_prompt.format_prompt(country=country)
-    result = llm(chat_prompt_with_values.to_messages())
-    json_data: dict[str, Any] = to_json(result.content)
+    chat_prompt_with_values = chat_prompt.format_prompt(
+        country=country_name, format_instructions=parser.get_format_instructions()
+    )
+    print(chat_prompt_with_values.to_messages())
+    output = llm(chat_prompt_with_values.to_messages())
+    country = parser.parse(output.content)
 
     # print the response
-    capital = json_data.get("capital", "Unknown")
-    print(f"The capital of {country} is {capital}.")
+    print(f"The capital of {country.name} is {country.capital}.")
 
 
 if __name__ == "__main__":
