75a92649370b10f649fda074fffdec059ee0e0cc
Arjan Egges <arjan@quarterfall.com>
2023-07-05 15:35:54 +0200

    Added missing types. Removed redundant import.

--- 2023/dunder/p1_custom_behavior.py
+++ 2023/dunder/p1_custom_behavior.py
@@ -1,15 +1,15 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
 
-    def __str__(self):
+    def __repr__(self) -> str:
         return f"Point({self.x}, {self.y})"
 
 
 def main() -> None:
-    p = Point(3, 4)
-    print(p)  # Output: Point(3, 4)
+    point = Point(3, 4)
+    print(point)  # Output: Point(3, 4)
 
 
 if __name__ == "__main__":
