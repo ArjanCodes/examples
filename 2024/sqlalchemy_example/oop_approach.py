b1b02d1b77fe4c5d09e8705e31759740afc8b7d5
MissArticulatePython <39281408+AlyceOsbourne@users.noreply.github.com>
2024-02-13 20:43:30 +0000

    Add files via upload

--- 2024/sqlalchemy_example/oop_approach.py
+++ 2024/sqlalchemy_example/oop_approach.py
@@ -2,27 +2,30 @@ import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 db = sa.create_engine('sqlite:///:memory:')
-Session = sessionmaker(bind=db)
+Session = sessionmaker(bind = db)
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'users'
-    
-    id: int = sa.Column(sa.Integer, primary_key=True)
-    username:str = sa.Column(sa.String)
+
+    id: int = sa.Column(sa.Integer, primary_key = True)
+    username: str = sa.Column(sa.String)
     email: str = sa.Column(sa.String)
-    
+
     def __repr__(self):
         return f'<User(username={self.username}, email={self.email})>'
-    
+
+
 def main():
     Base.metadata.create_all(db)
-    user = User(username='Arjan', email = "Arjan@arjancodes.com")
-    
+    user = User(username = 'Arjan', email = "Arjan@arjancodes.com")
+
     with Session() as session:
         session.add(user)
         session.commit()
         print(session.query(User).all())
-    
+
+
 if __name__ == '__main__':
     main()
