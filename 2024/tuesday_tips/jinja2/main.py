b9ff35ca6271febff2a83392b251eeba889cf915
Arjan Egges <arjan@quarterfall.com>
2024-04-12 13:47:54 +0200

    Added Jinja basic code from other project + initial email prompt.

--- 2024/tuesday_tips/jinja2/main.py
+++ 2024/tuesday_tips/jinja2/main.py
@@ -1,22 +1,39 @@
 import os
 
-from chat import chatter
 from dotenv import load_dotenv
+
+from ainode import chat, compose, debug, embed, jinja_template, merge, parse_json
+
+load_dotenv()
 
 
 def main() -> None:
-    load_dotenv()
-
-    openai_api_key = os.getenv("OPENAI_KEY")
-    if not openai_api_key:
+    api_key = os.getenv("OPENAI_KEY")
+    if not api_key:
         raise ValueError(
             "No API key found. Please set your OPENAI_KEY in the .env file."
         )
+    chat_fn = chat(api_key)
 
-    chat = chatter(api_key=openai_api_key)
+    quiz = {
+        "question_count": 5,
+        "quiz_difficulty": "easy",
+        "quiz_type": "mix",
+        "text": "Software design principles.",
+    }
 
-    response = chat("What is the capital of The Netherlands?")
-    print(response)
+    chain_fn = compose(
+        jinja_template("prompts/create_quiz.jinja"),
+        chat_fn,
+        debug,
+        parse_json,
+        embed("quiz"),
+        merge(quiz),
+        jinja_template("prompts/review_quiz.jinja"),
+        chat_fn,
+        debug,
+    )
+    chain_fn(quiz)
 
 
 if __name__ == "__main__":
