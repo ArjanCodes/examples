c2b0612db48a48caf04ad8a9a6f90da2f70c5e9b
Arjan Egges <arjan@quarterfall.com>
2024-06-05 15:36:02 +0200

    Updated code example so that all custom data operations are now moved to the declarative base.

--- 2024/apidesign/main.py
+++ 2024/apidesign/main.py
@@ -4,7 +4,7 @@ from typing import Any, Optional
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.params import Depends
-from pydantic import BaseModel, field_serializer, model_validator
+from pydantic import BaseModel
 from sqlalchemy import String, create_engine
 from sqlalchemy.orm import (
     DeclarativeBase,
@@ -12,11 +12,58 @@ from sqlalchemy.orm import (
     Session,
     mapped_column,
     sessionmaker,
+    validates,
 )
+
+DATABASE_URL = "sqlite:///test.db"
 
 
 class Base(DeclarativeBase):
-    pass
+    custom_data: Mapped[Optional[str]]
+
+    @validates("custom_data")
+    def validate_custom_data(self, _: str, custom_data: Any) -> str:
+        if not custom_data:
+            return "{}"
+        elif isinstance(custom_data, dict):
+            return json.dumps(custom_data)
+        else:
+            return custom_data
+
+    def update_item(self, key: str, value: Any) -> None:
+        if value is None:
+            return
+        if key == "custom_data":
+            self.update_custom_data(value)
+        else:
+            setattr(self, key, value)
+
+    def update(self, data: dict[str, Any]) -> None:
+        for key, value in data.items():
+            self.update_item(key, value)
+
+    def update_custom_data(self, custom_data: dict[str, Any]) -> None:
+        # create JSON of current custom_data
+        custom_data_json = json.loads(self.custom_data or "{}")
+        # unset keys with value None and update the rest
+        for k, v in custom_data.items():
+            if v is None:
+                custom_data_json.pop(k, None)
+            else:
+                custom_data_json[k] = v
+        # convert back to string
+        setattr(self, "custom_data", json.dumps(custom_data_json))
+
+    def dict(self) -> dict[str, Any]:
+        export_dict: dict[str, Any] = {}
+        for key, value in self.__dict__.items():
+            if key.startswith("_"):
+                continue
+            elif key == "custom_data":
+                export_dict[key] = json.loads(value)
+            else:
+                export_dict[key] = value
+        return export_dict
 
 
 class DBUser(Base):
@@ -24,13 +71,11 @@ class DBUser(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
     fullname: Mapped[Optional[str]]
-    custom_data: Mapped[Optional[str]]
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
 
 
-DATABASE_URL = "sqlite:///test.db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
@@ -44,25 +89,11 @@ class User(BaseModel):
     fullname: Optional[str] = None
     custom_data: Optional[dict[str, Any]] = None
 
-    @model_validator(mode="before")
-    @classmethod
-    def convert_custom_data(cls, data: dict[str, Any]) -> dict[str, Any]:
-        if "custom_data" in data:
-            data["custom_data"] = json.loads(data["custom_data"])
-        return data
-
 
 class UserCreate(BaseModel):
     name: str
     fullname: Optional[str] = None
     custom_data: Optional[dict[str, Any]] = None
-
-    @field_serializer("custom_data")
-    def serialize_custom_data(self, custom_data: Any) -> str | None:
-        print(custom_data)
-        if not custom_data:
-            return None
-        return json.dumps(custom_data)
 
 
 class UserUpdate(BaseModel):
@@ -85,7 +116,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)) -> User:
     db_user = db.query(DBUser).filter(DBUser.id == user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return User(**db_user.__dict__)
+    return User(**db_user.dict())
 
 
 @app.post("/users", response_model=User)
@@ -94,7 +125,7 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)) -> User:
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return User(**db_user.__dict__)
+    return User(**db_user.dict())
 
 
 @app.put("/users/{user_id}", response_model=User)
@@ -102,25 +133,10 @@ def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)) -
     db_user = db.query(DBUser).filter(DBUser.id == user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    for key, value in user.model_dump().items():
-        if value is None:
-            continue
-        if key == "custom_data":
-            # create JSON of current custom_data
-            custom_data_json = json.loads(db_user.custom_data or "{}")
-            # unset keys with value None and update the rest
-            for k, v in value.items():
-                if v is None:
-                    del custom_data_json[k]
-                else:
-                    custom_data_json[k] = v
-            # convert back to string
-            value = json.dumps(custom_data_json)
-        if value:
-            setattr(db_user, key, value)
+    db_user.update(user.model_dump())
     db.commit()
     db.refresh(db_user)
-    return User(**db_user.__dict__)
+    return User(**db_user.dict())
 
 
 @app.delete("/users/{user_id}", response_model=User)
@@ -130,7 +146,7 @@ def delete_user(user_id: int, db: Session = Depends(get_db)) -> User:
         raise HTTPException(status_code=404, detail="User not found")
     db.delete(db_user)
     db.commit()
-    return User(**db_user.__dict__)
+    return User(**db_user.dict())
 
 
 def main():
