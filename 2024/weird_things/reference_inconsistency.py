89fd992311f67b7e11d30dae99db76bf6f06b26a
Arjan Egges <arjan@quarterfall.com>
2024-08-21 10:36:25 +0200

    Added more examples.

--- 2024/weird_things/reference_inconsistency.py
+++ 2024/weird_things/reference_inconsistency.py
@@ -1,11 +1,20 @@
+def f():
+    return 1
+
+
 def main() -> None:
+    print(f())  # Output: 1
     a = [1, 2, 3]
     b = a  # b is now a reference to a
+    print(id(a))
+    print(id(b))
     a += [4]
     print(b)  # Output: [1, 2, 3, 4]
 
     s = "hello"
     t = s  # t is now a reference to s
+    print(id(s))
+    print(id(t))
     s += " world"
     print(t)  # Output: "hello"
 
