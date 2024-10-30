64650eca173252700304cc097f9e19d296cb37b0
Arjan Egges <arjan@quarterfall.com>
2024-10-30 15:39:11 +0100

    Added result example to IO container.

---
+++ 2024/returns/io_container_result.py
@@ -0,0 +1,27 @@
+from returns.io import IO
+from returns.result import safe
+
+
+@safe
+def read_file(file_path: str) -> IO[str]:
+    with open(file_path, "r") as file:
+        return IO(file.read())
+
+
+def process_data(data: str) -> str:
+    # Just a simple transformation for the example
+    return data.upper()
+
+
+def main() -> None:
+    file_io = read_file("user_dat.txt")
+    print(file_io)
+
+    # do something with the data in the IO container
+    processed_data = file_io.map(process_data)
+    processed_data.map(lambda data: print(data))
+    print(processed_data)
+
+
+if __name__ == "__main__":
+    main()
