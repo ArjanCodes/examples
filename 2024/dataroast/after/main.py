d095e92babf3c3d2c43d7164f1b4bc4fd28b2ae7
Arjan Egges <arjan@quarterfall.com>
2024-11-06 16:55:05 +0100

    Work in progress

--- 2024/dataroast/after/main.py
+++ 2024/dataroast/after/main.py
@@ -1,18 +1,13 @@
-from src.model.dictionary import DataDictionary
-from src.view.shell_view import Shell
-from src.controller.shell_controller import ShellController
-
 import pandas as pd
-
-
-
+from controller.shell_controller import ShellController
+from model.dictionary import DataDictionary
+from view.shell_view import Shell
 
 if __name__ == "__main__":
     model = DataDictionary()
 
-    model.create('file1', pd.read_csv('file1.csv'))
-    model.create('file2', pd.read_csv('file2.csv'))
-
+    model.create("file1", pd.read_csv("file1.csv"))
+    model.create("file2", pd.read_csv("file2.csv"))
 
     view = Shell(model)
 
