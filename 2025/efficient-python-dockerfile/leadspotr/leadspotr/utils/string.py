1e1b8c38d6dd81aa2d4e745fc0b032de5dca56b5
Arjan Egges <arjan@arjancodes.com>
2025-01-30 17:26:55 +0100

    WiP on improving dockerfile code example.

--- 2025/efficient-python-dockerfile/leadspotr/leadspotr/utils/string.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-
-def random_string(length: int = 8) -> str:
-    """Generate a random string of fixed length"""
-    characters = string.ascii_letters + string.digits
-    return "".join(random.choice(characters) for i in range(length))
