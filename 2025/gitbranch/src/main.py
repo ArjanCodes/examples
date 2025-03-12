426b1d37937aa6936b5d19c50c8aa35e26390431
Arjan Egges <arjan@arjancodes.com>
2025-03-12 16:41:32 +0100

    Added more examples.

--- 2025/gitbranch/src/main.py
+++ 2025/gitbranch/src/main.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+FEATURE_GOODBYES = True
+FEATURE_HELLO_NAME = True
 
-@app.get("/")
-def hello(name: str = "World"):
-    return f"Hello, {name}!"
+
+if FEATURE_HELLO_NAME:
+
+    @app.get("/")
+    def hello(name: str = "World"):
+        return f"Hello, {name}!"
+
+else:
+
+    @app.get("/")
+    def hello():
+        return "Hello, World!"
+
+
+if FEATURE_GOODBYES:
+
+    @app.get("/goodbye")
+    def goodbye():
+        return "Goodbye, World!"
--- 2025/gitbranch/tests/test_main.py
+++ 2025/gitbranch/tests/test_main.py
@@ -10,3 +10,17 @@ def test_hello():
     assert response.status_code == 200, response.text
     data = response.json()
     assert data == "Hello, World!"
+
+
+def test_hello_name():
+    response = client.get("/?name=Test")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data == "Hello, Test!"
+
+
+def test_goodbye():
+    response = client.get("/goodbye")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data == "Goodbye, World!"
