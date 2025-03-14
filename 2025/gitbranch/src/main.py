c660029dbf8f4c816c30d6eb9109552cacb1f401
Arjan Egges <arjan@arjancodes.com>
2025-03-14 14:57:30 +0100

    Code example cleanup. Added gitignore file updated main file and tests.

--- 2025/gitbranch/src/main.py
+++ 2025/gitbranch/src/main.py
@@ -2,25 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-FEATURE_GOODBYES = True
-FEATURE_HELLO_NAME = True
+FEATURE_GOODBYE = True
+FEATURE_FORMAL = True
 
 
-if FEATURE_HELLO_NAME:
+if FEATURE_FORMAL:
 
     @app.get("/")
-    def hello(name: str = "World"):
+    def hello_formal(name: str = "World", formal: bool = False):
+        if formal:
+            return f"Good day to you, {name}."
         return f"Hello, {name}!"
 
 else:
 
     @app.get("/")
-    def hello():
-        return "Hello, World!"
+    def hello(name: str = "World"):
+        return f"Hello, {name}!"
 
 
-if FEATURE_GOODBYES:
+if FEATURE_GOODBYE:
 
     @app.get("/goodbye")
-    def goodbye():
-        return "Goodbye, World!"
+    def goodbye(name: str = "World"):
+        return f"Goodbye, {name}!"
--- 2025/gitbranch/tests/test_main.py
+++ 2025/gitbranch/tests/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app
+from src.main import app
 
 # Setup the TestClient
 client = TestClient(app)
@@ -24,3 +24,17 @@ def test_goodbye():
     assert response.status_code == 200, response.text
     data = response.json()
     assert data == "Goodbye, World!"
+
+
+def test_goodbye_name():
+    response = client.get("/goodbye?name=Test")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data == "Goodbye, Test!"
+
+
+def test_hello_formal():
+    response = client.get("/?formal=True")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data == "Good day to you, World."
