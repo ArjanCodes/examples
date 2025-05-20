deb5c5fde9b18955a82927c1627860cc0b0bae1e
Arjan Egges <arjan@arjancodes.com>
2025-05-20 15:40:56 +0200

    Changed example to use 2nd gen cloud run functions. Cleanup.

--- 2025/serverless/__main__.py
+++ 2025/serverless/__main__.py
@@ -1,58 +1,44 @@
-import os
-import time
+from pulumi import asset, export
+from pulumi_gcp import cloudfunctionsv2, storage
 
-import pulumi
-from pulumi_gcp import cloudfunctions, storage
+bucket = storage.Bucket("bucket", location="europe-west1")
 
-# Disable rule for that module-level exports be ALL_CAPS, for legibility.
-# pylint: disable=C0103
-
-# File path to where the Cloud Function's source code is located.
-PATH_TO_SOURCE_CODE = "./functions"
-
-# We will store the source code to the Cloud Function in a Google Cloud Storage bucket.
-bucket = storage.Bucket("cf_demo_bucket", location="US", force_destroy=True)
-
-# The Cloud Function source code itself needs to be zipped up into an
-# archive, which we create using the pulumi.AssetArchive primitive.
-assets = {}
-for file in os.listdir(PATH_TO_SOURCE_CODE):
-    location = os.path.join(PATH_TO_SOURCE_CODE, file)
-    asset = pulumi.FileAsset(path=location)
-    assets[file] = asset
-
-archive = pulumi.AssetArchive(assets=assets)
-
-# Create the single Cloud Storage object, which contains all of the function's
-# source code. ("main.py" and "requirements.txt".)
-source_archive_object = storage.BucketObject(
-    "eta_demo_object",
-    name=f"main.py-{time.time()}",
+py_bucket_object = storage.BucketObject(
+    "python-zip",
     bucket=bucket.name,
-    source=archive,
+    source=asset.AssetArchive({".": asset.FileArchive("./function")}),
 )
 
-# Create the Cloud Function, deploying the source we just uploaded to Google
-# Cloud Storage.
-fxn = cloudfunctions.Function(
-    "eta_demo_function",
-    entry_point="hello_name",
-    region="us-central1",
-    runtime="python310",
-    source_archive_bucket=bucket.name,
-    source_archive_object=source_archive_object.name,
-    trigger_http=True,
+py_function = cloudfunctionsv2.Function(
+    "python-func",
+    name="python-func",
+    location="europe-west1",
+    build_config={
+        "runtime": "python313",
+        "entry_point": "channels_handler",
+        "source": {
+            "storage_source": {
+                "bucket": bucket.name,
+                "object": py_bucket_object.name,
+            },
+        },
+    },
+    service_config={
+        "max_instance_count": 1,
+        "available_memory": "256M",
+        "timeout_seconds": 60,
+    },
 )
 
-invoker = cloudfunctions.FunctionIamMember(
-    "invoker",
-    project=fxn.project,
-    region=fxn.region,
-    cloud_function=fxn.name,
+
+py_invoker = cloudfunctionsv2.FunctionIamMember(
+    "py-invoker",
+    project=py_function.project,
+    location=py_function.location,
+    cloud_function=py_function.name,
     role="roles/cloudfunctions.invoker",
+    # role="roles/run.invoker",
     member="allUsers",
 )
 
-# Export the DNS name of the bucket and the cloud function URL.
-pulumi.export("bucket_name", bucket.url)
-pulumi.export("fxn_url", fxn.https_trigger_url)
+export("python_endpoint", py_function.url)
