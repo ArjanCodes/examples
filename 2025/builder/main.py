64225b395988651aa16db16a8d5e56432a257f52
Arjan Egges <arjan@arjancodes.com>
2025-06-18 09:54:57 +0200

    Updated website. Made dataclass immutable. Added metadata to builder and page.

--- 2025/builder/main.py
+++ 2025/builder/main.py
@@ -9,10 +9,11 @@ def main() -> None:
         builder.set_title("Builder Pattern UI")
         .add_header("Hello from Python!", level=1)
         .add_paragraph("This page was generated using the Builder Pattern.")
-        .add_button("Visit ArjanCodes", onclick="https://arjan.codes")
+        .add_button("Visit ArjanCodes", onclick="https://www.arjancodes.com")
         .build()
     )
 
+    # --- Write to HTML File ---
     file_path = "page.html"
     with open(file_path, "w") as f:
         f.write(page.render())
