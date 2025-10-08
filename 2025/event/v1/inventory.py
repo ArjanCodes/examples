2ec14bb1efa5212d49ab2fff19a5ede1564d557e
Arjan Egges <arjan@arjancodes.com>
2025-10-08 15:54:20 +0200

    Updated event example

--- 2025/event/v1/inventory.py
+++ 2025/event/v1/inventory.py
@@ -1,30 +1,30 @@
 from collections import Counter
 from functools import cache
-from event_store import EventStore
 from event import Event, EventType
+from event_store import EventStore
 
 
 class Inventory:
-    def __init__(self, store: EventStore):
+    def __init__(self, store: EventStore[str]):
         self.store = store
 
     @cache
-    def get_items(self) -> list[str]:
-        start_items, events = self.store.get_replay_data()
-        counts = Counter(start_items)
-
-        for event in events:
+    def get_items(self) -> list[tuple[str, int]]:
+        counts = Counter[str]()
+        for event in self.store.get_all_events():
             if event.type == EventType.ITEM_ADDED:
-                counts[event.item] += 1
+                counts[event.data] += 1
             elif event.type == EventType.ITEM_REMOVED:
-                counts[event.item] -= 1
+                counts[event.data] -= 1
 
         return [
-            item
+            (item, count)
             for item, count in counts.items()
-            for _ in range(count)
             if count > 0
         ]
+
+    def get_count(self, item: str) -> int:
+        return dict(self.get_items()).get(item, 0)
 
     def _invalidate_cache(self) -> None:
         self.get_items.cache_clear()
@@ -34,7 +34,7 @@ class Inventory:
         self._invalidate_cache()
 
     def remove_item(self, item: str) -> None:
-        if item not in self.get_items():
+        if self.get_count(item) <= 0:
             raise ValueError(f"{item} not in inventory")
         self.store.append(Event(EventType.ITEM_REMOVED, item))
         self._invalidate_cache()
--- 2025/event/v1/main.py
+++ 2025/event/v1/main.py
@@ -3,15 +3,14 @@ from inventory import Inventory
 
 
 def main() -> None:
-    store = EventStore(snapshot_interval=5)
+    store = EventStore[str]()
     inventory = Inventory(store)
 
     inventory.add_item("sword")
     inventory.add_item("potion")
     inventory.add_item("bow")
     inventory.add_item("shield")
-    inventory.add_item("torch")  # Triggers snapshot
-
+    inventory.add_item("torch")
     inventory.remove_item("potion")
 
     print(inventory.get_items())  # ['sword', 'bow', 'shield', 'torch']
