f610db75db0bf57c1eec16b1e35f18b41b0558cd
Arjan Egges <arjan@arjancodes.com>
2026-03-18 15:59:13 +0100

    Simplified main file

--- 2026/apidata/main.py
+++ 2026/apidata/main.py
@@ -1,21 +1,9 @@
-# shop_api/main.py
-from __future__ import annotations
-
 from database import engine
 from db_models import Base
 from fastapi import FastAPI
 from routes import router
 
+Base.metadata.create_all(bind=engine)
 
-def create_app() -> FastAPI:
-    app = FastAPI(title="Shop API with custom_data")
-    app.include_router(router)
-    return app
-
-
-def main() -> FastAPI:
-    Base.metadata.create_all(bind=engine)
-    return create_app()
-
-
-app = main()
+app = FastAPI(title="Shop API")
+app.include_router(router)
